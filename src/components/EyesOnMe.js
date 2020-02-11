8db504b22525eb501e8f08523a69500a627dd154
KristinHannah <kristinhannahk@gmail.com>
2020-02-10 23:03:58 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+        }
+    }
+
+    focuser() {
+        console.log('Good!')
+    }
+
+    blurred() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <button onFocus={this.focuser} onBlur={this.blurred}></button>
+        )
+    }
+
+}
